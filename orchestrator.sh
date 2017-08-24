656512f403998eeec7ccf33cb20c5e801f651e01
Daniel Keirouz <daniel.keirouz@nist.gov>
2017-08-24 12:10:31 -0400

    added steps to delete wsm6 container

--- orchestrator.sh
+++ orchestrator.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo docker kill wsm6
+sudo docker rm wsm6
 sudo docker run --rm --name geogrid -v ~/WRF/data/:/root/WRF/data -v ~/WRF/WRF_NIST_Microservices_Public/namelist.wps:/root/WRF/WPS/namelist.wps wrf:geogrid
 sudo docker run --rm --name ungrib -v ~/WRF/data/:/root/WRF/data -v ~/WRF/WRF_NIST_Microservices_Public/namelist.wps:/root/WRF/WPS/namelist.wps wrf:ungrib ./ungrib.sh Vtable.GFS /root/WRF/data/FNL_2005_2007/fnl_200501\*
 sudo docker run --rm --name metgrid -v ~/WRF/data/:/root/WRF/data -v ~/WRF/WRF_NIST_Microservices_Public/namelist.wps:/root/WRF/WPS/namelist.wps wrf:metgrid
